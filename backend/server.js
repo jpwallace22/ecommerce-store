19766f9d1bb86d7b85530abf086c44f8e742b5bb
jpwallace22 <93415734+jpwallace22@users.noreply.github.com>
2022-01-02 10:53:16 -0800

    refactored backend error handling and installed redux/created & connected store.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from "express";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
 import { config } from "dotenv";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 config();
@@ -15,6 +15,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
